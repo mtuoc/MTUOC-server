fa3fad7055419e3504fb60b9df268ca7c5d48465
mtuoc <158745470+mtuoc@users.noreply.github.com>
2024-02-20 10:33:25 +0100

    Add files via upload

--- MTUOC_preprocess.py
+++ MTUOC_preprocess.py
@@ -137,7 +137,7 @@ def postprocess_segment(segmentPre):
             printLOG(1,"ERROR preprocess segment:",sys.exc_info())
     if config.isupper and config.truecase in ["upper","all"]:
         segmentPost=segmentPost.upper()
-    elif config.truecase in ["upper","all"]:
+    elif config.truecase in ["all"]:
         segmentPost=config.truecaser.truecase(segmentPost)
     if config.replace_EMAILs:
         segmentPost=restore_EMAILs(config.segmentORIG,segmentPost,code=config.code_EMAILs)
