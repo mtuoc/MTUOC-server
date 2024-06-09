55199e62a6d50ef506e20b4142618acc6a53d85b
mtuoc <158745470+mtuoc@users.noreply.github.com>
2024-06-09 13:56:03 +0200

    Add files via upload

---
+++ ApertiumTranslator.py
@@ -0,0 +1,34 @@
+#    ApertiumTranslator v 2406
+#    Description: an MTUOC server using Sentence Piece as preprocessing step
+#    Copyright (C) 2024  Antoni Oliver
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+import apertium
+
+class ApertiumTranslator:
+    def __init__(self, sl, tl):
+        self.sl=sl
+        self.tl=tl
+        
+    def translate(self,text):
+        self.translation=apertium.translate(self.sl, self.tl, text)
+        self.response={}
+        self.response["src"]=text
+        self.response["tgt"]=self.translation
+        self.response["alignments"]="None"
+        self.response["alternate_translations"]=[]
+        return(self.response)
+        
+
